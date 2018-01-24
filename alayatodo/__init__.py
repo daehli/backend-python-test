4e7106fb292ea73435904b15b5f4a9612355de48
daehli <daehlinadeauotis@gmail.com>
2018-01-23 22:01:15 -0500

    remove modals.html

--- alayatodo/__init__.py
+++ alayatodo/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask, g
 import sqlite3
-
+import os
 # configuration
-DATABASE = '/tmp/alayatodo.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/alayatodo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+if os.environ['PRODUCTION']:
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+  SECRET_KEY = os.environ['SECRET_KEY']
+  app.config.from_object(os.environ['APP_SETTINGS'])
+  DEBUG = False
+
+else:
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/alayatodo.db'
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+  SECRET_KEY = 'development key'
+  USERNAME = 'admin'
+  PASSWORD = 'default'
+  DEBUG = True
+  app.config.from_object(__name__)
+
 
 import alayatodo.views
